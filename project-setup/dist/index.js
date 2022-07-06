cb2cf3c41c33255b00f18ebb74324135770d2a6c
AleksBeliun <aleksandr@novoresume.com>
2022-07-06 17:33:11 +0200

    feat: Inputs as elements

--- project-setup/dist/index.js
+++ project-setup/dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
 const btn = document.querySelector('#my-btn');
-console.log("🚀 ~ file: index.ts ~ line 2 ~ btn", btn);
+const input = document.querySelector('#my-input');
+// (<HTMLInputElement>input).value = 'Hello World';
+console.log('🚀 ~ file: index.ts ~ line 2 ~ btn', btn);
+console.log('🚀 ~ file: index.ts ~ line 3 ~ input', input);
+btn.addEventListener('click', () => {
+    input.value = 'Hello World!';
+    console.log('🚀 ~ file: index.ts ~ line 5 ~ btn');
+});
--- project-setup/src/index.ts
+++ project-setup/src/index.ts
@@ -1,3 +1,11 @@
-const btn = document.querySelector('#my-btn');
-console.log("🚀 ~ file: index.ts ~ line 2 ~ btn", btn)
+const btn = document.querySelector('#my-btn')! as HTMLButtonElement;
+const input = document.querySelector('#my-input')! as HTMLInputElement;
+// (<HTMLInputElement>input).value = 'Hello World';
 
+console.log('🚀 ~ file: index.ts ~ line 2 ~ btn', btn);
+console.log('🚀 ~ file: index.ts ~ line 3 ~ input', input);
+
+btn.addEventListener('click', () => {
+    input.value = 'Hello World!';
+    console.log('🚀 ~ file: index.ts ~ line 5 ~ btn');
+});

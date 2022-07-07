254f01a207a30a3121fcb2b4995985e24509494f
AleksBeliun <aleksandr@novoresume.com>
2022-07-07 11:32:38 +0200

    feat: Basic ts class

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,1 +1,8 @@
 "use strict";
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+const player1 = new Player('John', 'Doe');
---
+++ classes/src/index.ts
@@ -0,0 +1,10 @@
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+    }
+}
+
+const player1 = new Player('John', 'Doe');

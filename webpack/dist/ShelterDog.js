169398da037a9e4075d813faaa38e46d9fa6697e
AleksBeliun <aleksandr@novoresume.com>
2022-07-09 11:42:30 +0200

    feat: config for production

--- webpack/dist/ShelterDog.js
+++
@@ -1,8 +0,0 @@
-import Dog from './Dog';
-export default class ShelterDog extends Dog {
-    constructor(name, breed, age, shelter) {
-        super(name, breed, age);
-        this.shelter = shelter;
-    }
-}
-//# sourceMappingURL=ShelterDog.js.map
